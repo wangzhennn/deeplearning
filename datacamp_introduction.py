bcc0e627111089a8039c74236a3273c2c607f06f
Zhen Wang <101124460+wangzhennn@users.noreply.github.com>
2022-11-26 16:10:13 +0100

    Update datacamp_introduction.py

--- datacamp_introduction.py
+++ datacamp_introduction.py
@@ -38,7 +38,7 @@ hidden_layer_outputs=np.array([node_0_output,node_1_output])
 output=(hidden_layer_output*weights['output']).sum()
 print(output)
 
-#=====example=======%
+#======example======%
 #The Rectified Linear Activation Function
 def relu(input):
     '''Define your relu activation function here'''
@@ -58,3 +58,61 @@ hidden_layer_outputs = np.array([node_0_output, node_1_output])
 model_output = (hidden_layer_outputs * weights['output']).sum()
 # Print model output
 print(model_output)
+
+#======example======%
+# Applying the network to many observations/rows of data
+# Define predict_with_network()
+def predict_with_network(input_data_row, weights):
+    # Calculate node 0 value
+    node_0_input = (input_data_row * weights['node_0']).sum()
+    node_0_output = relu(node_0_input)
+    # Calculate node 1 value
+    node_1_input = (input_data_row * weights['node_1']).sum()
+    node_1_output = relu(node_1_input)
+    # Put node values into array: hidden_layer_outputs
+    hidden_layer_outputs = np.array([node_0_output, node_1_output])
+    # Calculate model output
+    input_to_final_layer = (hidden_layer_outputs * weights['output']).sum()
+    model_output = relu(input_to_final_layer)
+    # Return model output
+    return(model_output)
+# Create empty list to store prediction results
+results = []
+for input_data_row in input_data:
+    # Append prediction to results
+    results.append(predict_with_network(input_data_row, weights))
+# Print results
+print(results)
+
+#--------------- 1.4 deeper networks --------------%
+#modeler doean't need to specify the interactions
+#when you train the model, the neural network gets weights that find the relevant patterns to make better predictions
+
+# Multi-layer neural networks
+def predict_with_network(input_data):
+    # Calculate node 0 in the first hidden layer
+    node_0_0_input = (input_data * weights['node_0_0']).sum()
+    node_0_0_output = relu(node_0_0_input)
+    # Calculate node 1 in the first hidden layer
+    node_0_1_input = (input_data * weights['node_0_1']).sum()
+    node_0_1_output = relu(node_0_1_input)
+    # Put node values into array: hidden_0_outputs
+    hidden_0_outputs = np.array([node_0_0_output, node_0_1_output])
+    # Calculate node 0 in the second hidden layer
+    node_1_0_input = (hidden_0_outputs * weights['node_1_0']).sum()
+    node_1_0_output = relu(node_1_0_input)
+    # Calculate node 1 in the second hidden layer
+    node_1_1_input = (hidden_0_outputs * weights['node_1_1']).sum()
+    node_1_1_output = relu(node_1_1_input)
+    # Put node values into array: hidden_1_outputs
+    hidden_1_outputs = np.array([node_1_0_output, node_1_1_output])
+    # Calculate output here: model_output
+    model_output = (hidden_1_outputs * weights['output']).sum()   
+    # Return model_output
+    return(model_output)
+output = predict_with_network(input_data)
+print(output)
+
+
+
+
