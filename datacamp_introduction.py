32c28fe6d46cfbd260151758eb6d557886422602
Zhen Wang <101124460+wangzhennn@users.noreply.github.com>
2022-11-28 15:11:11 +0100

    Update datacamp_introduction.py

--- datacamp_introduction.py
+++ datacamp_introduction.py
@@ -114,5 +114,85 @@ output = predict_with_network(input_data)
 print(output)
 
 #--------------- 2.1 the need for optimization --------------%
+#the change in weights can improve the model for the data point
+#loss function: agregates errors in predictions from many data points into single number. measure of model's predictive performance
+
+# squared error
+
+# gradient descent
+#- start at random point
+#- until you are somewhere flat: find the slope, take a step downhill
+
+#======Example======%
+# Coding how weight changes affect accuracy
+# The data point you will make a prediction for
+input_data = np.array([0, 3])
+# Sample weights
+weights_0 = {'node_0': [2, 1],
+             'node_1': [1, 2],
+             'output': [1, 1]
+            }
+# The actual target value, used to calculate the error
+target_actual = 3
+# Make prediction using original weights
+model_output_0 = predict_with_network(input_data, weights_0)
+# Calculate error: error_0
+error_0 = model_output_0 - target_actual
+# Create weights that cause the network to make perfect prediction (3): weights_1
+weights_1 = {'node_0': [2, 1],
+             'node_1': [1, 2],
+             'output': [1, 0]
+            }
+# Make prediction using new weights: model_output_1
+model_output_1 = predict_with_network(input_data, weights_1)
+# Calculate error: error_1
+error_1 = model_output_1 - target_actual
+# Print error_0 and error_1
+print(error_0)
+print(error_1)
+
+#======Example======%
+#Scaling up to multiple data points
+from sklearn.metrics import mean_squared_error
+# Create model_output_0 
+model_output_0 = []
+# Create model_output_0
+model_output_1 = []
+# Loop over input_data
+for row in input_data:
+    # Append prediction to model_output_0
+    model_output_0.append(predict_with_network(row, weights_0))
+    # Append prediction to model_output_1
+    model_output_1.append(predict_with_network(row, weights_1))
+# Calculate the mean squared error for model_output_0: mse_0
+mse_0 = mean_squared_error(target_actuals, model_output_0)
+# Calculate the mean squared error for model_output_1: mse_1
+mse_1 = mean_squared_error(target_actuals, model_output_1)
+# Print mse_0 and mse_1
+print("Mean squared error with weights_0: %f" %mse_0)
+print("Mean squared error with weights_1: %f" %mse_1)
+
+#--------------- 2.2 Gradient descent --------------%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
