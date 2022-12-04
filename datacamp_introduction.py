0461ecdd30901e8b99f65425a378128ef4efafc8
Zhen Wang <101124460+wangzhennn@users.noreply.github.com>
2022-12-04 18:20:06 +0100

    Update datacamp_introduction.py

--- datacamp_introduction.py
+++ datacamp_introduction.py
@@ -173,12 +173,60 @@ print("Mean squared error with weights_0: %f" %mse_0)
 print("Mean squared error with weights_1: %f" %mse_1)
 
 #--------------- 2.2 Gradient descent --------------%
+#if the slope is positive
+#going opposite the slope means moving to lower numbers
+#subtract the slop from the current value
+#too big a steop might lead us astray
 
+#solution: learning rate--update each weight by subtracting learning rate*slope
 
+#slope calculation
+#need to multiply
+#- slope of the loss function w.r.t value at teh node we feed into --> slope of mean-squared loss function w.r.t predictino: 2(predicted value -actual value=2error)
+#- the value of the node that feeds into our weight --> 
+#- slope of the activition function w.r.t value we feed into
 
+#code to calculate slopes and update weights
+import numpy as np
+weights = np.arrary([1,2])
+input_data=np.array([3,4])
+target=6
+learning_rate=0.01
+preds=(weights*input_data).sum()
+error=preds-target
+print(error)
 
+gradient=2*input_data*error
+gradient
 
+weights_updated=weights-learning_rate*gradient
+preds_updates=(weights_updated*input_data).sum()
+error_updated=pred_updated-target
+print(error_updated)
 
+#===example===%
+n_updates = 20
+mse_hist = []
+
+# Iterate over the number of updates
+for i in range(n_updates):
+    # Calculate the slope: slope
+    slope = get_slope(input_data, target, weights)
+    
+    # Update the weights: weights
+    weights = weights - 0.01 * slope
+    
+    # Calculate mse with new weights: mse
+    mse = get_mse(input_data, target, weights)
+    
+    # Append the mse to mse_hist
+    mse_hist.append(mse)
+
+# Plot the mse history
+plt.plot(mse_hist)
+plt.xlabel('Iterations')
+plt.ylabel('Mean Squared Error')
+plt.show()
 
 
 
